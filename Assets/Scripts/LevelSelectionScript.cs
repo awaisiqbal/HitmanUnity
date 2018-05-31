da36023c740d8b637e4ee5712ea933d6f93dafbc
Awais Iqbal <awais.iqbal2@gmail.com>
2018-05-31 15:35:46 +0200

    Fixed bug loading level

--- Assets/Scripts/LevelSelectionScript.cs
+++ Assets/Scripts/LevelSelectionScript.cs
@@ -6,42 +6,68 @@ using UnityEngine.SceneManagement;
 public class LevelSelectionScript : MonoBehaviour
 {
     public GameObject levelSelectionMenu;
+    public bool isLoadingLevel = false;
 
     public void PlayLevel1()
     {
+        if (isLoadingLevel)
+        {
+            return;
+        }
         unloadScene();
         StaticInformation.CurrentLevel = 1;
         setStarted();
+        isLoadingLevel = true;
         SceneManager.LoadSceneAsync(1);
     }
 
     public void PlayLevel2()
     {
+        if (isLoadingLevel)
+        {
+            return;
+        }
         unloadScene();
         StaticInformation.CurrentLevel = 2;
         setStarted();
+        isLoadingLevel = true;
         SceneManager.LoadSceneAsync(2);
     }
 
     public void PlayLevel3()
     {
+        if (isLoadingLevel)
+        {
+            return;
+        }
         unloadScene();
         StaticInformation.CurrentLevel = 3;
         setStarted();
+        isLoadingLevel = true;
         SceneManager.LoadSceneAsync(3);
     }
     public void PlayLevel4()
     {
+        if (isLoadingLevel)
+        {
+            return;
+        }
         unloadScene();
         StaticInformation.CurrentLevel = 4;
         setStarted();
+        isLoadingLevel = true;
         SceneManager.LoadSceneAsync(4);
     }
     public void PlayLevel5()
     {
+        if (isLoadingLevel)
+        {
+            return;
+        }
         unloadScene();
         StaticInformation.CurrentLevel = 5;
         setStarted();
+        isLoadingLevel = true;
         SceneManager.LoadSceneAsync(5);
     }
 
